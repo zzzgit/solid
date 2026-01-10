1b8e856887ffdac7b28a6dffaa0a83d274395577
zzzgit <zzzgoo@gmail.com>
2026-01-10 19:54:52 +0800

    222

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,11 @@
-import { splitProps } from 'solid-js'
+import { mergeProps, splitProps } from 'solid-js'
 import { css, cx } from '@emotion/css'
+
+const defaults = {
+	variant: 'solid',
+	size: 'md',
+
+}
 
 const Button = (props)=> {
 	const [local, others] = splitProps(props, [
@@ -10,8 +16,18 @@ const Button = (props)=> {
 		'className',
 	])
 
+	// 計算size的class
+	const validSizes = ['xs', 'sm', 'md', 'lg', 'xl', 'xxl']
+	const sizeName = validSizes.includes(local.size) ? local.size : defaults.size
+	const colorName = props.color || 'blue'
+	const variantName = local.variant || defaults.variant
+
 	return (
-		<button class={containerStyle(props)} {...others}>
+		<button
+			classList={{ [sizeName]: true, [variantName]: true }}
+			class={containerStyle({ sizeName, colorName })}
+			{...others}
+		>
 			{local.children}
 		</button>
 	)
@@ -19,11 +35,50 @@ const Button = (props)=> {
 
 export default Button
 
-const containerStyle = props=> css`
---text-color: var(--${props.color}-9);
---bg-color: var(--${props.color}-3);
---border-color: var(--${props.color}-7);
-color: var(--text-color);
-background-color: var(--bg-color);
-border: 1px solid var(--border-color);
+const containerStyle = ({ sizeName, colorName })=> css`
+--solid-background: var(--${colorName}-9);
+--solid-hover-background: var(--${colorName}-10);
+--outline-border: var(--${colorName}-a7);
+--outline-foreground: var(--${colorName}-a11);
+--outline-hover-background: var(--${colorName}-a2);
+--surface-background: var(--${colorName}-2);
+--surface-hover-background: var(--${colorName}-3);
+--surface-border-color: var(--${colorName}-6);
+--surface-hover-border-color: var(--${colorName}-7);
+--surface-foreground: var(--${colorName}-11);
+
+height: var(--component-height-${sizeName});
+min-height: var(--component-height-${sizeName});
+padding: var(--component-padding-${sizeName});
+font-size: var(--component-font-size-${sizeName});
+border-radius: 4px;
+border: none;
+cursor: pointer;	
+
+&.solid {
+	background-color: var(--solid-background);
+	color: white;
+	border-color: var(--accent-color);
+	&:hover {
+		background-color: var(--solid-hover-background);
+	}
+}
+&.outline {
+	border: 1px solid var(--outline-border);
+	background-color: transparent;
+	color: var(--outline-foreground);
+	&:hover {
+		background-color: var(--outline-hover-background);
+	}
+}
+&.surface {
+	border: 1px solid var(--surface-border-color);
+	background-color: var(--surface-background);
+	color: var(--surface-foreground);
+	&:hover {
+		background-color: var(--surface-hover-background);
+		border-color: var(--surface-hover-border-color);
+	}
+}
+
 `
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -26,7 +26,28 @@ export default function About(){
 					<li>建立高效的開發流程</li>
 				</ul>
 			</div>
-			<Button color='red'>聯繫我們</Button>
+			<div style={{
+				'margin-top': '30px', display: 'flex', gap: '10px',
+			}}>
+				<Button color='red' variant='solid'>聯繫我們</Button>
+				<Button color='red' variant='surface'>聯繫我們</Button>
+				<Button color='red' variant='outline'>聯繫我們</Button>
+				<Button color='red' variant='solid'>聯繫我們</Button>
+				<Button color='red' size='lg'>聯繫我們</Button>
+				<Button color='red' size='sm'>聯繫我們</Button>
+
+			</div>
+			<div style={{
+				'margin-top': '30px', display: 'flex', gap: '10px',
+			}}>
+				<Button color='blue' size='sm'variant='solid'>聯繫我們</Button>
+				<Button color='blue' size='md'variant='surface'>聯繫我們</Button>
+				<Button color='blue' size='md'variant='outline'>聯繫我們</Button>
+				<Button color='blue' size='lg'variant='solid'>聯繫我們</Button>
+				<Button color='blue' size='lg'>聯繫我們</Button>
+				<Button color='blue' size='xl'>聯繫我們</Button>
+
+			</div>
 		</div>
 	)
 }
